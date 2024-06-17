71539b05a66325ed798ca269dd7de3e5b991a80a
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-06-17 16:44:11 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
+    match pattern {
+        _ if pattern.chars().count() == 1 => input_line.contains(pattern),
+        "\\d" => input_line.chars().any(|c| c.is_ascii_digit()),
+        _ => panic!("Pattern not found: {}", pattern),
     }
 }
 
